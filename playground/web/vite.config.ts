f33f03c6d372854f0631c8967b44924c260e6c32
qp91abc1234 <350018736@qq.com>
2024-07-31 13:51:28 +0800

    feat: html 模板压缩插件制作

---
+++ playground/web/vite.config.ts
@@ -0,0 +1,42 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import ViteHtmlTemplate from 'vite-html-template'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/vite-pro/',
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
+    ViteHtmlTemplate({
+      inject: {
+        ['index.html']: {
+          data: {
+            ver: 'vite-pro',
+          },
+        },
+      }
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/common/scss/mixin.scss';`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+})
---
+++ viteHtmlTemplate/src/index.ts
@@ -0,0 +1,81 @@
+import type { ResolvedConfig } from 'vite'
+import type { HtmlTagDescriptor } from 'vite'
+import type { Options as EJSOptions } from 'ejs'
+import { render } from 'ejs'
+import path from 'node:path'
+import type { Options as MinifyOptions } from 'html-minifier-terser'
+import { minify as minifyFn } from 'html-minifier-terser'
+
+interface IInjectData {
+  data?: Record<string, any>
+  tags?: HtmlTagDescriptor[]
+  ejsOptions?: EJSOptions
+}
+
+interface IOptions {
+  minify?: boolean | MinifyOptions
+  inject?: { [key: string]: IInjectData }
+}
+
+export default function ViteHtmlTemplate(opts: IOptions) {
+  let minify = opts.minify
+  const injectData = opts.inject ?? {}
+  let config: ResolvedConfig
+
+  if(minify === null || minify === undefined || minify === true) {
+    minify = {
+      collapseWhitespace: true,
+      keepClosingSlash: true,
+      removeComments: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      useShortDoctype: true,
+      minifyCSS: true,
+    }
+  }
+
+  return [{
+      name: 'vite-html-template',
+      configResolved(cfg) {
+        config = cfg
+      },
+      transformIndexHtml: {
+        order: 'pre',
+        handler: async (html, ctx) => {
+          const htmlName = path.basename(ctx.filename)
+          const param = injectData[htmlName]
+          const ejsData: Record<string, any> = {
+            ...config.env,
+            ...(config.define ?? {}),
+            ...(param.data ?? {}),
+          }
+          const finalHtml = await render(html, ejsData, param.ejsOptions)
+          return {
+            html: finalHtml,
+            tags: param.tags ?? [],
+          }
+        },
+      },
+    }, {
+      name: 'vite-html-compress',
+      enforce: 'post',
+      apply: 'build',
+      async generateBundle(_, bundle) {
+        if (minify === false) return
+        const keys = Object.keys(bundle)
+        for (let i = 0; i < keys.length; i++) {
+          const key = keys[i]
+          const item = bundle[key]
+          if (
+            item.type === 'asset' &&
+            item.fileName.endsWith('.html') &&
+            typeof item.source === 'string'
+          ) {
+            item.source = await minifyFn(item.source, minify)
+          }
+        }
+      },
+    }
+  ]
+}
